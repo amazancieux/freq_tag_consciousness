7ea656e1c905b7848955af608d9d728d1b7920e6
Audrey Mazancieux <audrey.mazancieux@gmail.com>
2024-02-02 16:15:21 +0100

    Update parameters

--- FreqTagStim/FreqTagStim_parameters.py
+++ FreqTagStim/FreqTagStim_parameters.py
@@ -9,13 +9,14 @@ tagging task.
 
 """
 
-# Other devices
+# Main infos
 
+SESSION             = 1 # 1 or 2 
 IsEEG               = 0
 
 # Stimuli caracteristics
 
-CONTRASTS           = [0.5, 1, 1.5] # sublinal, threashold, and supraliminal contrasts
+CONTRASTS           = [3] # sublinal, threashold, and supraliminal contrasts
 NSEQ_PER_CONTRATS   = 1
 
 # Presentation caracteristics
@@ -23,14 +24,14 @@ NSEQ_PER_CONTRATS   = 1
 REFRESH_RATE        = 60 # in Hz
 STIM_FREQ           = 6 # in Hz
 FADE                = 1.6667 # in sec
-NB_STIM_SEQ         = 20
+NB_STIM_SEQ         = 240
 NB_TARGET_FIXCROSS  = 6 # mean number of detections in the attentional task
-DUR_CHANGE_FIXCROSS = 3 # duration of change for fix cross (in number of stim)
+DUR_CHANGE_FIXCROSS = 4 # duration of change for fix cross (in number of stim)
 
 # Constants for colours
 
 BLACK               = (0, 0, 0)
-GRAY_B              = (119, 136, 153)
+RED                 = (255, 0, 0)
 BLUE                = (0, 0, 255)
 DARK_GREY           = (96, 96, 96)
 WHITE               = (255, 255, 255)
